f4926b9c6bf4cbedf98fce0c438ffba388d1f25f
Kien Dinh <kiendinh@Kiens-Mac-mini.local>
2024-11-09 21:53:24 -0700

    3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Logo from '../images/logo.svg'
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={Logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
