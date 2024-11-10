16a776df100386c3d866ca19a5651dad6670e481
Kien Dinh <kiendinh@Kiens-Mac-mini.local>
2024-11-10 09:35:24 -0700

    message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from '../images/logo.svg'
-import { pageLinks } from '../data'
+import { pageLinks, socialLinks } from '../data'
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -25,6 +25,16 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
+          {socialLinks.map((social) => {
+            const { id, href, icon } = social
+            return (
+              <li key={id}>
+                <a href={href} target="_blank" className="nav-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
+            )
+          })}
           <li>
             <a
               href="https://www.twitter.com"
@@ -34,24 +44,7 @@ const Navbar = () => {
               <i className="fab fa-facebook"></i>
             </a>
           </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          <li></li>
         </ul>
       </div>
     </nav>
